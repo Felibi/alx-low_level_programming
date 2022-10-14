97399ad5208055aa70614162df5d8940fa10bc5a
Felix I. Felix <felibif@gmail.com>
2022-10-14 07:26:44 -0700

    task low level 0-3

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+/* this program prints a message to the standard output*/
+#include <stdio.h>
+main()
+{
+	puts(""Programming is like building a multilingual puzzle.\n");
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+/*This program just prints a message*/
+#include <stdio.h>
+main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
