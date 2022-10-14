54dd01f8e70a2c2443cff26da2b6d28e4975e441
Felix I. Felix <felibif@gmail.com>
2022-10-14 10:19:14 -0700

    my first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
  * main - Displays a message using puts function
  * return - 0 always for success
  */
+#include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
