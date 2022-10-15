b47514370ed53a111f6ba3d3006b04df1baa2fc2
Felix I. Felix <felibif@gmail.com>
2022-10-14 17:55:19 -0700

    in quotes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,17 @@
 #include <stdio.h>
 int main(void)
 {
+	char xter;
 	int integer;
-	float floating;
 	long int longint;
 	long long int long2;
-	char xter;
+	float floating;
 
 	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(xter));
 	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(integer));
 	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(longint));
 	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(long2));
 	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(floating));
-	
+
 	return (0);
 }
