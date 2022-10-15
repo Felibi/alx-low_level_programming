e108f0c539534c30e41d65da282f166ad388405d
Felix I. Felix <felibif@gmail.com>
2022-10-14 18:08:24 -0700

    new size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,13 @@ int main(void)
 	char xter;
 	int integer;
 	long int longint;
-	long long int long2;
+	long long int lg2;
 	float floating;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(xter));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(integer));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longint));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(long2));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lg2));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floating));
 
 	return (0);
