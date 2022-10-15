6fbafd0ce05205bbb63ec4b3c441be24608cd8a4
Felix I. Felix <felibif@gmail.com>
2022-10-15 08:49:59 -0700

    grit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive");
+		printf("%d", n "is positive");
 	} else if (n == 0)
 	{
-		printf("is zero");
+		printf("%d", n "is zero");
 	} else
 	{
-		printf("is negative");
+		printf("%d", n "is negative");
 	}
 
 	return (0);
