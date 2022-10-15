1c5f635a53586fc7743f5035d790994fb7df83de
Felix I. Felix <felibif@gmail.com>
2022-10-14 17:21:22 -0700

    task advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,8 @@
  * main - displays a message to standard error.
  * return: 1
  */
+#include <stdio.h>
+#include <unistd.h>
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,17 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("Size of int: %ld bytes\n", sizeof(int));
-	printf("Size of float: %ld bytes\n", sizeof(float));
-	printf("Size of long int: %ld bytes\n", sizeof(long int));
-	printf("Size of long long int: %ld bytes\n", sizeof(long long int));
-	printf("Size of char: %ld byte\n", sizeof(char));
+	int integer;
+	float floating;
+	long int longint;
+	long long int long2;
+	char xter;
+
+	printf("Size of int: %lu bytes\n", (unsigned long)sizeof(integer));
+	printf("Size of float: %lu bytes\n", (unsigned long)sizeof(floating));
+	printf("Size of long int: %lu bytes\n", (unsigned long)sizeof(longint));
+	printf("Size of long long int: %lu bytes\n", (unsigned long)sizeof(long2));
+	printf("Size of char: %lu byte\n", (unsigned long)sizeof(xter));
 
 	return (0);
 }
