e93f714d9752f14b5aa319a8951017f5b1e2c5cb
Felix I. Felix <felibif@gmail.com>
2022-10-16 11:43:08 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - display 0-9 with putchar.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int b10;
+
+	for (b10 = 0; b10 < 10; b10++)
+	{
+		putchar(b10);
+	}
+	putchar('\n');
+	return (0);
+}
