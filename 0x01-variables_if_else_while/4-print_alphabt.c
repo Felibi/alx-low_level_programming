7b5c3f92168a6c97673a69c3e9da29c04f9a0da0
Felix I. Felix <felibif@gmail.com>
2022-10-16 07:50:02 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - display alphabets excluding q and e.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alph = 'a';
+	char alph != 'q', 'e';
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
+
