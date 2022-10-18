322e7068b7d64247c08e8b334448e51adbd7d8ce
Felix I. Felix <felibif@gmail.com>
2022-10-17 19:28:28 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,27 +8,49 @@ int main(void)
 {
 	int k, h;
 
-	for (k = 48; k < 58; k++)
+	k = 48;
+
+	while (k < 58)
 	{
-		for (h = 48; h < 58; h++)
+		h = 48;
+
+		while (h < 58)
 		{
-			if (k == h)
-			{
-				continue;
-			}
 			putchar(k);
 			putchar(h);
-			if (k == 56 && h == 57)
-			{
-				break;
-			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			h++;
+			putchar(',');
+			putchar(' ');
 		}
+		k++;
 	}
 	putchar('\n');
 	return (0);
 }
+/**
+ *
+ *	for (k = 48; k < 58; k++)
+ *	{
+ *		for (h = 48; h < 58; h++)
+ *		{
+ *			if (k == h)
+ *			{
+ *				continue;
+ *			}
+ *			putchar(k);
+ *			putchar(h);
+ *			if (k == 56 && h == 57)
+ *			{
+ *				break;
+ *			}
+ *			else
+ *			{
+ *				putchar(',');
+ *				putchar(' ');
+ *			}
+ *		}
+ *	}
+ *	putchar('\n');
+ *	return (0);
+ *}
+ */
