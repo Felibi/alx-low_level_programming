6e7465b381b0aa8660682265b04a3bc9eb12d55b
Felix I. Felix <felibif@gmail.com>
2022-10-20 09:44:22 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,6 +20,18 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
+	else if (a > c && c > b)
+	{
+		largest = a;
+	}
+	else if (b > c && c > a)
+	{
+		largest = b;
+	}
+	else if (c > a && a > b)
+	{
+		largest = c;
+	}
 	else
 	{
 		largest = c;
