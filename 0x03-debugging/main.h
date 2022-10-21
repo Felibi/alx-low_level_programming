d2f19d4ad1ea3bda48aadd4a532052b439f1955b
Felix I. Felix <felibif@gmail.com>
2022-10-21 02:07:20 -0700

    task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,16 +3,8 @@
 
 #include <stdio.h>
 
-#include "main.h"
-
 void positive_or_negative(int i);
 
-int main(void)
-
-int largest_number(int a, int b, int c)
-
-void print_remaining_days(int month, int day, int year)
-
-int convert_day(int month, int day)
+int largest_number(int a, int b, int c);
 
 #endif
