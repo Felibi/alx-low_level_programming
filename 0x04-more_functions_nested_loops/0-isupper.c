a73bc8488dac2e30c077c4d327bd352665b20ba5
Felix I. Felix <felibif@gmail.com>
2022-10-21 14:52:18 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks if character is uppercase
+ *
+ * Return: 1 if upppercase or 0 for anything else
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
