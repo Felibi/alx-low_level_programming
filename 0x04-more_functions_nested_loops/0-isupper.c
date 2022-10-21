7b8b4e1f67239c16bb2d6c5bf468f03b8eab1ac9
Felix I. Felix <felibif@gmail.com>
2022-10-21 15:43:45 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - checks if character is uppercase
+ * _isupper -> checks if character is uppercase
  *@c: an integer parametre
  * Return: 1 if upppercase or 0 for anything else
  */
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN.H
 #define MAIN.H
-
 int _putchar(char c);
 int _isupper(int c);
 
