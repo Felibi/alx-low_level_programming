6256391c1c57a2280f17256aa98bbd81e40f7ea5
Felix I. Felix <felibif@gmail.com>
2022-10-21 18:31:44 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0 to 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int k;
+
+	k = 0;
+	while (k < 10)
+	{
+		if (k == 2 && k == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(k + '0');
+		}
+	}
+	_putchar('\n');
+}
