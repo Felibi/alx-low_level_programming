4b6ef71121961bcd448dfe51f89de711e23e5111
Felix I. Felix <felibif@gmail.com>
2022-10-21 18:06:43 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	_putchar('0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+	_putchar('\n');
+}
