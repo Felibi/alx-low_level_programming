7bced53dc5e09abe68c85a08e916defa6a8b9f68
Felix I. Felix <felibif@gmail.com>
2022-10-21 19:38:18 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 14 ten times
+ */
+void more_numbers(void)
+{
+	int z, k;
+
+	for (z = 0; z < 10; z++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			_putchar(k + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
