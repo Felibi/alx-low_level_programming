7fb5533f523d0e90c1c84e02d655b624f3e9b665
Felix I. Felix <felibif@gmail.com>
2022-10-21 20:34:02 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ * @n: line char
+ */
+void print_line(int n)
+{
+	int k;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (k = 0; k <= n; k++)
+		_putchar(' ');
+	_putchar('\n');
+}
