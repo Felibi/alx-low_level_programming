f4711a271d41cc67e2539f2658ad2c1a004fa80d
Felix I. Felix <felibif@gmail.com>
2022-10-24 02:02:54 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char alphTEN, k;
+
+	for (k = 0; k <= 10; K++)
+	{
+		for (alphTEN = 'a'; alphTEN <= 'z'; alphTEN++)
+		{
+			_putchar(alphTEN);
+		}
+	}
+	_putchar('\n');
+
+}
