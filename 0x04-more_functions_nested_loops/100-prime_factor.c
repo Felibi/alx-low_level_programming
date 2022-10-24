0fc9e2a17bad592ae1185b107806e5fbaee64cad
Felix I. Felix <felibif@gmail.com>
2022-10-23 23:37:34 -0700

    task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,13 +7,35 @@
 
 int main(void)
 {
-	unsigned long int i, n = 612852475143;
-
-	for (i = 3; i < 782849; i = i + 2)
+	long int i = 2, n = 612852475143;
+	long int ans = 0, hiFACT; 
+	
+	while (n != 0)
 	{
-		while ((n % i == 0) && (n != i))
+		if (n % i != 0)
+			i = i + 1;
+		
+		else
+		{
+			hiFACT = n;
 			n = n / i;
+		
+			if (n == 1)
+			{
+				printf("%li\n", n)
+				ans = 1;
+				break;
+			}
+		}
 	}
-	printf("%lu\n", n);
 	return (0);
 }
+/**
+*	for (i = 3; i < 782849; i = i + 2)
+*	{
+*		while ((n % i == 0) && (n != i))
+*			n = n / i;
+*	}
+*	printf("%lu\n", n);
+*	return (0);
+*/
