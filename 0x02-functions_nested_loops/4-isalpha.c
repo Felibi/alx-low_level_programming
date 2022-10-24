04fa360b9877727894ae50d5d0cc5146f55af0ef
Felix I. Felix <felibif@gmail.com>
2022-10-24 03:04:54 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha(int c)
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' || 'A' && c <= 'z' || 'Z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
