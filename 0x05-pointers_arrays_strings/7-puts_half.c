01321c6f2b7fe5ecc693119005469ed89640e669
Felix I. Felix <felibif@gmail.com>
2022-10-27 16:49:04 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half part of a string.
+ * @str: the string to print
+ */
+void puts_half(char *str)
+{
+	int x, y, i;
+
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (i = y; i < x; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
